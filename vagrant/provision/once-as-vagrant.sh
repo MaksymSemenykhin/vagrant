5a352cab318d70f136b8f09f8682806785963f15
Maksym Semenykhin <semenihin.maksim@gmail.com>
2018-06-23 12:59:52 +0300

    .gitattributes updated and remove all sh files

--- vagrant/provision/once-as-vagrant.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-source /home/vagrant/provision/output.sh
-source /home/vagrant/provision/functions.sh
-source /home/vagrant/provision/config_read.sh
-
-print_title "Provision-script user: `whoami`"

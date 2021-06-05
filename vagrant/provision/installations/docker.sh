ae593345fb038d75eef2d96a4de22d047dc66bea
Semenykhin Maksym <>
2021-06-05 15:50:52 +0300

    <bug>Fix docker-compose install script misplaced sudo

--- vagrant/provision/installations/docker.sh
+++ vagrant/provision/installations/docker.sh
@@ -60,6 +60,6 @@ sudo usermod -aG docker vagrant
 
 print_title "Docker compose install"
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-composesudo
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
